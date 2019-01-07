e5a72fd8b937a9c393a50f3e206efee50c8dfda1
Banana Viking <bananavikingcoder@gmail.com>
2019-01-07 16:19:29 -0600

    added word, usedLetters, and print statements to main()

--- Hangman/main.m
+++ Hangman/main.m
@@ -8,10 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+void printWord(NSString *word, NSMutableArray<NSString*> *usedLetters) {
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSString *word = @"RHYTHM";
+        NSMutableArray<NSString*> *usedLetters = [NSMutableArray arrayWithCapacity:8];
+        
+        printf("Welcome to Hangman!\n");
+        printf("Press a letter to guess, or Ctrl+C to quit.\n");
+        printWord(word, usedLetters);
     }
     return 0;
 }
