a999ec988d5d90c0a4e959ac02ffe98f61b5dc1c
Banana Viking <bananavikingcoder@gmail.com>
2019-01-07 16:52:20 -0600

    added logic to printWord to check and print game state

--- Hangman/main.m
+++ Hangman/main.m
@@ -9,7 +9,45 @@
 #import <Foundation/Foundation.h>
 
 void printWord(NSString *word, NSMutableArray<NSString*> *usedLetters) {
+    printf("\nWord: ");
     
+    // this will be used to track missing letters
+    BOOL missingLetters = NO;
+    
+    // loop over ever letter
+    for (NSInteger i = 0; i < [word length]; ++i) {
+        // convert the unichar into an NSString for arrays
+        unichar letter = [word characterAtIndex:i];
+        NSString *letterString = [NSString stringWithFormat:@"%C", letter];
+        
+        // if we already guessed this letter, print it out
+        if ([usedLetters containsObject:letterString]) {
+            printf("%C", letter);
+        } else {
+            // letter not guessed; mark with a placeholder
+            printf("_");
+            
+            // we haven't won yet!
+            missingLetters = YES;
+        }
+    }
+    
+    printf("\nGuesses: %ld/8\n", [usedLetters count]);
+    
+    if (missingLetters == NO) {
+        // no missing letter - a winner!
+        printf("It looks like you live on... for now.\n");
+        exit(0);
+    } else {
+        if ([usedLetters count] == 8) {
+            // they guessed eight times; game over
+            printf("Oops - you died! The word was %s.\n", [word cStringUsingEncoding:NSUTF8StringEncoding]);
+            exit(0);
+        } else {
+            // game is still active
+            printf("Enter a guess: ");
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
