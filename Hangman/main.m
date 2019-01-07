cb425d5ac5ed42b89bbf2334f7c742f5a524b5f1
Banana Viking <bananavikingcoder@gmail.com>
2019-01-07 16:13:17 -0600

    Initial Commit

---
+++ Hangman/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hangman
+//
+//  Created by Banana Viking on 1/7/19.
+//  Copyright © 2019 Banana Viking. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
